cea7fe6bbd3495e905b2437dfc7bc254974cf781
AnastasiaStr <star-nastia18@yandex.ua>
2019-03-27 20:05:31 +0200

    Initial commit.

---
+++ PhotosViewer/PhotosViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosViewer
+//
+//  Created by Anastasia on 3/27/19.
+//  Copyright © 2019 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
